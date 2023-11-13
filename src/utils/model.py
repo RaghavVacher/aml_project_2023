76a91f0dafd2d27cce5a295c5346945ad94ae7ca
rvacher <rvacher@xnovotech.com>
2023-11-13 15:37:05 +0100

    Add model utils

---
+++ src/utils/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+def get_pretrained_regression_model(output_size):
+    # Load the pretrained ResNet18 model
+    pretrained_model = models.resnet18(weights='DEFAULT')
+    
+    # Modify the last fully connected layer for regression with custom output size
+    in_features = pretrained_model.fc.in_features
+    pretrained_model.fc = nn.Linear(in_features, output_size)
+    
+    return pretrained_model
+
+model = get_pretrained_regression_model(100)
