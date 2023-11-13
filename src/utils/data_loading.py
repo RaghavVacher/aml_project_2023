b291c9470a6126ae5de7b54af71f955327c9ec95
rvacher <rvacher@xnovotech.com>
2023-11-13 15:37:58 +0100

    Add CustomDataset Class Utils

--- src/utils/data_loading.py
+++ src/utils/data_loading.py
@@ -1,5 +1,9 @@
+from torch.utils.data import Dataset, DataLoader
+from sklearn.decomposition import PCA 
+from torchvision import transforms
 import numpy as np
 from PIL import Image
+import torch
 import os
 
 def load_images_from_folder(folder_path, start=None, end=None):
@@ -18,3 +22,51 @@ def load(subject, index_start=None, index_end=None):
     folder_path = path+"/training_split/training_images/"
     image_data = load_images_from_folder(folder_path, index_start, index_end)
     return data_lh, data_rh, image_data
+
+class ImaginaryDataset(Dataset):
+    def __init__(self, images_list, outputs_list, transform=None, PCA=None):
+        self.num_samples = len(images_list)
+        self.transform = transform
+        self.PCA = PCA
+        self.data, self.output = self.load_data(images_list, outputs_list)
+
+    def load_data(self, images_list, outputs_list):
+        data = []
+        output_concat = []
+
+        for i in range(self.num_samples):
+            # Load image from the given list
+            image = Image.fromarray(images_list[i])
+
+            # Load output array from the given list
+            output = outputs_list[i]
+
+            data.append((image, output))
+            output_concat.append(output)
+
+        if self.PCA:
+            self.PCA.fit(output_concat)
+
+        return data, output_concat
+
+    def give_output(self):
+        return self.output
+
+    def internal_PCA(self):
+        if self.PCA:
+            return self.PCA
+        else:
+            print(f'PCA is {self.PCA}')
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        image, output = self.data[idx]
+
+        if self.transform:
+            image = self.transform(image)
+        if self.PCA:
+            output = self.PCA.transform(output.reshape(1, -1))
+
+        return image, torch.FloatTensor(output[0])
