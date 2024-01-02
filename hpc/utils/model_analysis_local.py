b07322c2dce35c4a355f2efb4f68b920ada8c0d9
dpl373 <112472730+dpl373@users.noreply.github.com>
2024-01-02 10:36:59 +0100

    combined corr plot all subj

--- hpc/utils/model_analysis_local.py
+++ hpc/utils/model_analysis_local.py
@@ -77,41 +77,6 @@ def make_prediction(model, flattened_dict, in_feat_model):
     
     return preds
 
-
-# def make_prediction(model, flattened_dict, in_feat_model, subject):
-#     preds = {}
-#     for key, item in flattened_dict.items():
-#         adj_layer = torch.nn.Linear(len(item), in_feat_model)
-#         print('3. in_size of adj_layer', len(item))
-#         print('4. out_size of adj_layer', in_feat_model)
-#         print('5. in_size of shared layer', model.shared.in_features)
-#         adj_layer.requires_grad = False
-#         adj_output = adj_layer(item)
-#         shared = model.shared(adj_output)
-#         if subject == 1:
-#             subject = model.sub1(adj_output)
-#         elif subject == 2:
-#             subject = model.sub2(adj_output)
-#         elif subject == 3:
-#             subject = model.sub3(adj_output)
-#         elif subject == 4:
-#             subject = model.sub4(adj_output)
-#         elif subject == 5:
-#             subject = model.sub5(adj_output)
-#         elif subject == 6:
-#             subject = model.sub6(adj_output)
-#         elif subject == 7:
-#             subject = model.sub7(adj_output)
-#         elif subject == 8:
-#             subject = model.sub8(adj_output)
-
-#         # Average the shared and subject-specific layers
-#         combined = (shared + subject) / 2
-        
-#         pred = model.head(combined)
-#         preds[key] = pred
-#     return preds
-
 def get_pca_model(subject):
     pca = joblib.load(f'/Users/emilykruger/Documents/GitHub/aml_project_2023/hpc/utils/pca_models/pca_model_subj0{subject}.joblib')
     return pca
@@ -318,63 +283,66 @@ def corr_roi_plot_allsub(subject_list, model, split = True):
         # Correlate each predicted LH vertex with the corresponding ground truth vertex
         for v in tqdm(range(lh.shape[1])):
             lh_correlation[v] = pearsonr(lh_data_pred[:,v], lh[:,v])[0]
-        correlations_lh.append(lh_correlation)
 
         # Correlate each predicted RH vertex with the corresponding ground truth vertex
         for v in tqdm(range(rh.shape[1])):
             rh_correlation[v] = pearsonr(rh_data_pred[:,v], rh[:,v])[0]
-        correlations_rh.append(rh_correlation)
 
-    lh_correlation = np.mean(np.stack(correlations_lh), axis = 0)
-    rh_correlation = np.mean(np.stack(correlations_rh), axis = 0)
+        # Load the ROI classes mapping dictionaries
+        roi_mapping_files = ['mapping_prf-visualrois.npy', 'mapping_floc-bodies.npy',
+            'mapping_floc-faces.npy', 'mapping_floc-places.npy',
+            'mapping_floc-words.npy', 'mapping_streams.npy']
+        roi_name_maps = []
+        for r in roi_mapping_files:
+            roi_name_maps.append(np.load(os.path.join(working_dir, 'roi_masks', r),
+                allow_pickle=True).item())
 
-    # Load the ROI classes mapping dictionaries
-    roi_mapping_files = ['mapping_prf-visualrois.npy', 'mapping_floc-bodies.npy',
-        'mapping_floc-faces.npy', 'mapping_floc-places.npy',
-        'mapping_floc-words.npy', 'mapping_streams.npy']
-    roi_name_maps = []
-    for r in roi_mapping_files:
-        roi_name_maps.append(np.load(os.path.join(working_dir, 'roi_masks', r),
-            allow_pickle=True).item())
+        # Load the ROI brain surface maps
+        lh_challenge_roi_files = ['lh.prf-visualrois_challenge_space.npy',
+            'lh.floc-bodies_challenge_space.npy', 'lh.floc-faces_challenge_space.npy',
+            'lh.floc-places_challenge_space.npy', 'lh.floc-words_challenge_space.npy',
+            'lh.streams_challenge_space.npy']
+        rh_challenge_roi_files = ['rh.prf-visualrois_challenge_space.npy',
+            'rh.floc-bodies_challenge_space.npy', 'rh.floc-faces_challenge_space.npy',
+            'rh.floc-places_challenge_space.npy', 'rh.floc-words_challenge_space.npy',
+            'rh.streams_challenge_space.npy']
+        lh_challenge_rois = []
+        rh_challenge_rois = []
+        for r in range(len(lh_challenge_roi_files)):
+            lh_challenge_rois.append(np.load(os.path.join(working_dir, 'roi_masks',
+                lh_challenge_roi_files[r])))
+            rh_challenge_rois.append(np.load(os.path.join(working_dir, 'roi_masks',
+                rh_challenge_roi_files[r])))
 
-    # Load the ROI brain surface maps
-    lh_challenge_roi_files = ['lh.prf-visualrois_challenge_space.npy',
-        'lh.floc-bodies_challenge_space.npy', 'lh.floc-faces_challenge_space.npy',
-        'lh.floc-places_challenge_space.npy', 'lh.floc-words_challenge_space.npy',
-        'lh.streams_challenge_space.npy']
-    rh_challenge_roi_files = ['rh.prf-visualrois_challenge_space.npy',
-        'rh.floc-bodies_challenge_space.npy', 'rh.floc-faces_challenge_space.npy',
-        'rh.floc-places_challenge_space.npy', 'rh.floc-words_challenge_space.npy',
-        'rh.streams_challenge_space.npy']
-    lh_challenge_rois = []
-    rh_challenge_rois = []
-    for r in range(len(lh_challenge_roi_files)):
-        lh_challenge_rois.append(np.load(os.path.join(working_dir, 'roi_masks',
-            lh_challenge_roi_files[r])))
-        rh_challenge_rois.append(np.load(os.path.join(working_dir, 'roi_masks',
-            rh_challenge_roi_files[r])))
+        # Select the correlation results vertices of each ROI
+        roi_names = []
+        lh_roi_correlation = []
+        rh_roi_correlation = []
+        for r1 in range(len(lh_challenge_rois)):
+            for r2 in roi_name_maps[r1].items():
+                if r2[0] != 0: # zeros indicate to vertices falling outside the ROI of interest
+                    roi_names.append(r2[1])
+                    lh_roi_idx = np.where(lh_challenge_rois[r1] == r2[0])[0]
+                    rh_roi_idx = np.where(rh_challenge_rois[r1] == r2[0])[0]
+                    lh_roi_correlation.append(lh_correlation[lh_roi_idx])
+                    rh_roi_correlation.append(rh_correlation[rh_roi_idx])
+        roi_names.append('All vertices')
+        lh_roi_correlation.append(lh_correlation)
+        rh_roi_correlation.append(rh_correlation)
 
-    # Select the correlation results vertices of each ROI
-    roi_names = []
-    lh_roi_correlation = []
-    rh_roi_correlation = []
-    for r1 in range(len(lh_challenge_rois)):
-        for r2 in roi_name_maps[r1].items():
-            if r2[0] != 0: # zeros indicate to vertices falling outside the ROI of interest
-                roi_names.append(r2[1])
-                lh_roi_idx = np.where(lh_challenge_rois[r1] == r2[0])[0]
-                rh_roi_idx = np.where(rh_challenge_rois[r1] == r2[0])[0]
-                lh_roi_correlation.append(lh_correlation[lh_roi_idx])
-                rh_roi_correlation.append(rh_correlation[rh_roi_idx])
-    roi_names.append('All vertices')
-    lh_roi_correlation.append(lh_correlation)
-    rh_roi_correlation.append(rh_correlation)
-
-    # Create the plot
-    lh_mean_roi_correlation = [np.mean(lh_roi_correlation[r])
-        for r in range(len(lh_roi_correlation))]
-    rh_mean_roi_correlation = [np.mean(rh_roi_correlation[r])
-        for r in range(len(rh_roi_correlation))]
+        # Create the plot
+        lh_mean_roi_correlation = [np.mean(lh_roi_correlation[r])
+            for r in range(len(lh_roi_correlation))]
+        rh_mean_roi_correlation = [np.mean(rh_roi_correlation[r])
+            for r in range(len(rh_roi_correlation))]
+        
+        #append subject-specific ROI corr to list
+        correlations_lh.append(lh_mean_roi_correlation)
+        correlations_rh.append(rh_mean_roi_correlation)
+        print(f'Subject {subject} done.')
+    
+    lh_correlation = np.mean(np.array(correlations_lh), axis = 0)
+    rh_correlation = np.mean(np.array(correlations_rh), axis = 0)
 
     plt.figure(figsize=(18,6))
     x = np.arange(len(roi_names))
